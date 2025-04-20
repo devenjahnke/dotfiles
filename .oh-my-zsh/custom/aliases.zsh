767750fe7d53fce4b0ce180d08a3dddfcce7852a
Deven Jahnke <devenjahnke@Devens-MacBook-Pro.local>
2025-04-20 17:24:16 -0400

    fix: convert composer alias to function

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,8 @@
-alias composer="docker run --rm --interactive --tty \
-  --volume $PWD:/app \
-  --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp \
-  composer"
+composer() {
+  docker run --rm -it \
+    --volume "$PWD:/app" \
+    --volume "${COMPOSER_HOME:-$HOME/.composer}:/tmp" \
+    --workdir /app \
+    composer "$@"
+}
+
