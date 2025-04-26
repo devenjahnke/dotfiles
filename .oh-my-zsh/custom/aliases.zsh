aaf12b8155edbd3879f51f0f7c3eaa56a417de28
Deven Jahnke <10586846+devenjahnke@users.noreply.github.com>
2025-04-26 14:21:40 -0400

    feat: Add helm alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -6,3 +6,13 @@ composer() {
     composer "$@"
 }
 
+helm() {
+  docker run --rm -it \
+  	--volume "$PWD:/app" \
+	--volume "$HOME/.kube:/root/.kube" \
+	--volume "$HOME/.helm:/root/.helm" \
+	--volume "$HOME/.config/helm:/root/.config/helm" \
+	--volume "$HOME/.cache/helm:/root/.cache/helm" \
+	--workdir /app \
+	alpine/helm "$@"
+}
